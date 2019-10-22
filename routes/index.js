678aa2208b7b68bec6b155428bbaae4bb40c4ff9
Simar Singh <simar@codejudge.io>
2019-10-22 16:02:24 +0530

    project working

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/api/add', function(req, res, next) {
+  let number1 = req.body.number1;
+  let number2 = req.body.number2;
+  let sum = number1 + number2;
+  return res.send({"sum": sum});
+});
+
 module.exports = router;
